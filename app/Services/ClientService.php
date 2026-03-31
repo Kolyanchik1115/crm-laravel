2febd0480a623a5630e3fb97e2191b8cb2ae78a9
Sprat <105858796+Kolyanchik1115@users.noreply.github.com>
2026-03-31 23:38:14 +0200

    feat: task 8 - added cache

--- app/Services/ClientService.php
+++ app/Services/ClientService.php
@@ -6,10 +6,16 @@ namespace App\Services;
 use App\Models\Client;
 use App\Repositories\ClientRepository;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class ClientService
 {
     protected ClientRepository $repository;
+
+    // Cache key
+    private const string CACHE_KEY_CLIENTS = 'crm:clients:list';
+    // TTL - 2 min
+    private const int CACHE_TTL = 120;
 
     public function __construct(ClientRepository $repository)
     {
@@ -17,12 +23,26 @@ class ClientService
     }
 
     /**
-     * Get all clients
+     * Get all clients (with cache)
      */
     public function getAllClients(): Collection
     {
-        return $this->repository->getAll();
+        $cached = Cache::get(self::CACHE_KEY_CLIENTS);
+
+        // If there is a cache, and it is of the correct type
+        if ($cached instanceof Collection) {
+            return $cached;
+        }
+
+        // If there is no cache, or it is corrupted, we take it from the database
+        $clients = $this->repository->getAll();
+
+        // Save in cache
+        Cache::put(self::CACHE_KEY_CLIENTS, $clients, self::CACHE_TTL);
+
+        return $clients;
     }
+
 
     /**
      * Get client by ID
@@ -33,7 +53,7 @@ class ClientService
     }
 
     /**
-     * Create new client
+     * Create new client and invalidate cache
      */
     public function createClient(array $data): Client
     {
@@ -42,6 +62,11 @@ class ClientService
         $data['currency'] = $data['currency'] ?? config('crm.default_currency', 'UAH');
         $data['is_active'] = $data['is_active'] ?? true;
 
-        return $this->repository->create($data);
+        $client = $this->repository->create($data);
+
+        // invalidate data
+        Cache::forget(self::CACHE_KEY_CLIENTS);
+
+        return $client;
     }
 }
